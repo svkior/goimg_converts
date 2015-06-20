9e5500bec804ec2631cb537e0e86ae8cd457beff
Sergey V. Kior <svkior@gmail.com>
2015-06-20 12:03:01 +0300

    qwe

--- main.go
+++ main.go
@@ -3,9 +3,6 @@ package main
 import (
 	"fmt"
 	"log"
-)
-
-import (
 	"github.com/lxn/walk"
 	. "github.com/lxn/walk/declarative"
 	"io/ioutil"
@@ -13,6 +10,7 @@ import (
 	//"image/color"
 	"image"
 	"os/exec"
+	"math"
 )
 
 func main() {
@@ -52,6 +50,20 @@ func main() {
 	}
 }
 
+
+func rgb2l(r,g,b uint32) (uint32){
+	var l, rf, gf, bf, max, min float64
+
+	rf = math.Max(math.Min(float64(r)/65535,1), 0)
+	gf = math.Max(math.Min(float64(g)/65535,1), 0)
+	bf = math.Max(math.Min(float64(b)/65535,1), 0)
+
+	max = math.Max(rf, math.Max(gf,bf))
+	min = math.Min(rf, math.Min(gf,bf))
+	l = (max + min) / 2
+	return uint32(l*100)
+}
+
 type MyMainWindow struct {
 	*walk.MainWindow
 	model *ImgModel
@@ -62,7 +74,7 @@ type MyMainWindow struct {
 
 func (mw *MyMainWindow) lb_CurrentIndexChanged() {
 	i := mw.lb.CurrentIndex()
-	if i > 0 {
+	if i > 1 {
 		return
 	}
 
@@ -82,15 +94,15 @@ func (mw *MyMainWindow) lb_CurrentIndexChanged() {
 	width := srcImage.Bounds().Dx()
 	height := srcImage.Bounds().Dy()
 
-	expWidth := fmt.Sprintf("--export-width=%d", width)
-	expHeight := fmt.Sprintf("--export-height=%d",height)
+	expWidth := fmt.Sprintf("--export-width=%d", int(float32(width) * 0.6) )
+	//expHeight := fmt.Sprintf("--export-height=%d",height)
 
 	out, err := exec.Command(
 		"C:\\Program Files\\Inkscape\\inkscape.exe",
 		"TTS-watermark-white.svg",
 		"--export-png=out.png",
 		expWidth,
-		expHeight,
+		//expHeight,
 	).Output()
 
 	if err != nil {
@@ -104,12 +116,78 @@ func (mw *MyMainWindow) lb_CurrentIndexChanged() {
 		return
 	}
 
+	// Нужно расчитать куда выводить ватермарку
+	wmWidth := waterImg.Bounds().Dx()
+	wmHeight := waterImg.Bounds().Dy()
+
+	wmBeginX := (width - wmWidth)/2
+	wmBeginY := (height - wmHeight)/2
+
+	wmBeginY2 := int(float32(height)* 0.2) - wmHeight/2
+	wmBeginY3 := int(float32(height)* 0.8) - wmHeight/2
+
+
 	// Делаем Blur
 	bluredImg := imaging.Blur(waterImg, 10)
 
-	waterMarked := imaging.Overlay(srcImage, bluredImg, image.Pt(0, 0), 0.20)
+	// Определеяем какого цвета будем делать Watermark
+	grImg := imaging.Grayscale(srcImage)
 
-	imaging.Save(waterMarked, "test.jpg")
+	Summ := uint32(0)
+	Count := uint32(0)
+
+	for idx :=0; idx < grImg.Bounds().Dx(); idx++ {
+		for idy := 0; idy < grImg.Bounds().Dy(); idy++ {
+			oldPixel := grImg.At(idx, idy)
+			r, g, b, _ := oldPixel.RGBA()
+			//log.Printf("%d %d %d %d", r, g, b, a)
+			Summ += rgb2l(r,g,b)
+			Count ++
+		}
+	}
+	//log.Printf("Summ: %d, Count: %d, mean: %d", Summ, Count, Summ/Count)
+	meann := Summ/Count
+
+
+
+	waterMarked := imaging.Overlay(srcImage, bluredImg, image.Pt(wmBeginX, wmBeginY), 0.15)
+
+	mMk2 := imaging.Overlay(waterMarked, bluredImg, image.Pt(wmBeginX, wmBeginY2), 0.15)
+
+	mMk3 := imaging.Overlay(mMk2, bluredImg, image.Pt(wmBeginX, wmBeginY3), 0.15)
+
+	expWidth2 := fmt.Sprintf("--export-width=%d", int(float32(width) * 0.2) )
+	out, err = exec.Command(
+		"C:\\Program Files\\Inkscape\\inkscape.exe",
+		"TTS-watermark-white.svg",
+		"--export-png=out2.png",
+		expWidth2,
+		//expHeight,
+	).Output()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Stdout: %s", out)
+	waterImg, _ = imaging.Open("out2.png")
+
+
+	wmBeginX4 := int(float64(width)*0.9) - waterImg.Bounds().Dx()
+	wmBeginY4 := int(float64(height)*0.9) - waterImg.Bounds().Dy()
+
+
+	if(meann > 50){
+		log.Printf("Mean: %d", meann)
+		waterImg = imaging.Invert(waterImg)
+		mMk4 := imaging.Overlay(mMk3, waterImg, image.Pt(wmBeginX4, wmBeginY4), 1.0)
+		imaging.Save(mMk4, "test.jpg")
+	} else {
+		log.Printf("Mean: %d", meann)
+		mMk4 := imaging.Overlay(mMk3, waterImg, image.Pt(wmBeginX4, wmBeginY4), 1.0)
+		imaging.Save(mMk4, "test.jpg")
+	}
+
+
 
 	img, err := walk.NewImageFromFile("test.jpg")
 	if err != nil {
